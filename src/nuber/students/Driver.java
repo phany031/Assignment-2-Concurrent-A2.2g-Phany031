7f9cc49c19683a6dd6ca5ddb9f39ddf4a42d896c
phany031 <phany031@mymail.unisa.edu.au>
2024-10-16 22:55:57 +1030

    Call superclass constructor in Driver to initialize name and maxSleep

--- src/nuber/students/Driver.java
+++ src/nuber/students/Driver.java
@@ -2,11 +2,10 @@ package nuber.students;
 
 public class Driver extends Person {
 
-	
-	public Driver(String driverName, int maxSleep)
-	{
+	public Driver(String driverName, int maxSleep) {
+		super(driverName, maxSleep);
 	}
-	
+
 	/**
 	 * Stores the provided passenger as the driver's current passenger and then
 	 * sleeps the thread for between 0-maxDelay milliseconds.
@@ -14,17 +13,17 @@ public class Driver extends Person {
 	 * @param newPassenger Passenger to collect
 	 * @throws InterruptedException
 	 */
-	public void pickUpPassenger(Passenger newPassenger)
-	{
+	public void pickUpPassenger(Passenger newPassenger) {
+
 	}
 
 	/**
-	 * Sleeps the thread for the amount of time returned by the current 
-	 * passenger's getTravelTime() function
+	 * Sleeps the thread for the amount of time returned by the current passenger's
+	 * getTravelTime() function
 	 * 
 	 * @throws InterruptedException
 	 */
 	public void driveToDestination() {
 	}
-	
+
 }
